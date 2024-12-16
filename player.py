5a8c1f5c5250eba811b07d1bd2701c6dc291ac26
weisskaiser <matheuskrausemk@outlook.com>
2024-12-16 07:31:24 -0300

    adds player movement

--- player.py
+++ player.py
@@ -22,10 +22,18 @@ class Player(circleshape.CircleShape):
     def rotate(self, dt):
         self.rotation += dt * constants.PLAYER_TURN_SPEED
     
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * constants.PLAYER_SPEED * dt
+    
     def update(self, dt):
         keys = pygame.key.get_pressed()
 
+        if keys[pygame.K_w]:
+            self.move(dt)
+        if keys[pygame.K_s]:
+            self.move(-dt)
         if keys[pygame.K_a]:
-            self.rotate(-1 * dt)
+            self.rotate(-dt)
         if keys[pygame.K_d]:
             self.rotate(dt)
