18fcf4afa4e6585e2cf98f0d10d4171ba282f48b
weisskaiser <matheuskrausemk@outlook.com>
2024-12-16 07:02:04 -0300

    creates repo, main, constants

---
+++ main.py
@@ -0,0 +1,19 @@
+import pygame
+import constants
+
+def main():
+    print("Starting asteroids!")
+    print(f"Screen height: {constants.SCREEN_HEIGHT}")
+    print(f"Screen width: {constants.SCREEN_WIDTH}")
+    pygame.init()
+    screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill((0, 0, 0))
+        pygame.display.flip()
+
+if __name__ == '__main__':
+    main()
