324e1d4e66152c532dc3cebcb00c832826eda58c
weisskaiser <matheuskrausemk@outlook.com>
2024-12-16 07:07:31 -0300

    adds clock to main, and circleshape

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ def main():
     print(f"Screen width: {constants.SCREEN_WIDTH}")
     pygame.init()
     screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         for event in pygame.event.get():
@@ -14,6 +16,7 @@ def main():
                 return
         screen.fill((0, 0, 0))
         pygame.display.flip()
+        dt = clock.tick(60)/1000
 
 if __name__ == '__main__':
     main()
